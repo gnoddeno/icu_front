33ae07daf8737bf7fbf09be0ca999989d16b705c
Ubuntu <ubuntu@ip-172-31-9-234.ap-northeast-2.compute.internal>
2024-05-24 06:32:25 +0000

    2024-05-24

---
+++ server.js
@@ -0,0 +1,26 @@
+import http from "http";
+import express from 'express';
+import path from"path";
+
+const app = express();
+
+const port = 80; //인스턴스 생성시 만들었던 포트번호 기입
+
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", (req, res) => {
+  res.set({
+    "Cache-Control": "no-cache, no-store, must-revalidate",
+    Pragma: "no-cache",
+    Date: Date.now()
+  });
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+http.createServer(app).listen(port, () => {
+  console.log(`app listening at ${port}`);
+});
